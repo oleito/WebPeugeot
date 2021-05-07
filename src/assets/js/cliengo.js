bae948c11b9d54e53962b8e20379562034496d54
Leandro <oleito@gmail.com>
2021-05-07 00:38:19 -0300

    cliengo funcionando. ok

---
+++ src/assets/js/cliengo.js
@@ -0,0 +1,8 @@
+(function () {
+    var ldk = document.createElement('script');
+    ldk.type = 'text/javascript';
+    ldk.async = true;
+    ldk.src = 'https://s.cliengo.com/weboptimizer/5e7bb9d9e4b0cad9a131f5e7/5e7bcecbe4b0cad9a131fd45.js?platform=view_installation_code';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ldk, s);
+})();
